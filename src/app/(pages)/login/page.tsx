2ea954e0855b29fe5b63a5147357329edad8ed87
chipmunkcol <jack95711@gmail.com>
2025-01-16 23:43:11 +0900

    design: 로그인 & 회원가입 완료 Page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,29 +1,57 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function Page() {
   console.log('로그인 페이지 렌더링')
-  const searchParams = useSearchParams()
+  // const searchParams = useSearchParams()
   const router = useRouter()
 
   useEffect(() => {
-    const token = searchParams.get('token')
-    if (token) {
-      router.push('/onboarding')
-    }
+    // const token = searchParams.get('token')
+    // if (token) {
+    //   router.push('/onboarding')
+    // }
   }, [])
+
+  const onClickLogin = () => {
+    router.push('/onboarding')
+  }
 
   return (
     <div>
-      <div>hello, login!</div>
-      <div>
-        {/* <button onClick={loginHandler}>카카오 로그인</button> */}
-        <button>
-          <Link href={'http://localhost:8080/oauth2/authorization/kakao'}>카카오 로그인</Link>
-        </button>
+      <div
+        className=" flex flex-col justify-center items-center gap-10 "
+        style={{ height: '100vh' }}
+      >
+        <div className="">
+          <Image width={126} height={44} src={'/images/logo/BitO.svg'} alt="logo" />
+        </div>
+        <div className="text-center" style={{ fontSize: '21px' }}>
+          <div>간편하게 로그인하고</div>
+          <div>다양한 서비스를 이용해보세요.</div>
+        </div>
+        <div
+          className="mt-4 mb-10 cursor-pointer"
+          onClick={() => {
+            onClickLogin()
+          }}
+        >
+          <Image
+            width={340}
+            height={52}
+            src={'/images/icon/kakaoBtn.svg'}
+            alt="카카오톡-로그인-버튼"
+          />
+        </div>
+        <div>
+          <div className="underline text-sm" style={{ color: '#787878' }}>
+            다른 이메일로 시작하기
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/app/(pages)/onboarding/page.tsx
+++ src/app/(pages)/onboarding/page.tsx
@@ -1,18 +1,45 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Page() {
   return (
     <div>
-      <div>BitO </div>
-      <div>Icon</div>
-      <div>Icon</div>
-      <div>회원가입 완료</div>
-      <div>***님 반갑습니다!</div>
-      <div>회원이 되신것을 환영합니다.</div>
-      <div>
-        <button>
-          <Link href={''}>시작하기</Link>
-        </button>
+      <div
+        className="flex flex-col justify-center items-center gap-24 text-center"
+        style={{ height: ' 100vh' }}
+      >
+        <div className="flex flex-col gap-5">
+          <div>
+            <Image width={126} height={44} src={'/images/logo/BitO.svg'} alt="logo" />
+          </div>
+          <div className="flex justify-center">
+            <Image width={50} height={50} src={'/images/logo/icon-50.svg'} alt="logo-icon" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <div className="flex justify-center">
+            <Image width={50} height={50} src={'/images/icon/check_br.png'} alt="chcek-icon" />
+          </div>
+          <div style={{ fontSize: '21px' }}>회원가입 완료</div>
+          <div>
+            <div>이**님 반갑습니다!</div>
+            <div>회원이 되신것을 환영합니다.</div>
+          </div>
+        </div>
+        <div className="border-r">
+          <button
+            style={{
+              width: '345px',
+              height: '52px',
+              borderRadius: '40px',
+              backgroundColor: '#C5B697',
+              color: '#fff',
+              fontSize: '16px',
+            }}
+          >
+            시작하기
+          </button>
+        </div>
       </div>
     </div>
   )

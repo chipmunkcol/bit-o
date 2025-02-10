d7e681a10ba47ce1e0bf056667fbbb0eada063c8
chipmunkcol <jack95711@gmail.com>
2025-02-10 22:40:51 +0900

    feat: 각 페이지 바로기 링크 추가 (index_page )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <span>홈페이지</span>
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl text-center">임시 페이지입니다🙌</h1>
+      <div className="flex flex-col gap-5 mt-10 text-center">
+        <Link href={'/login'}>
+          <div className="hover:text-gray-500">로그인 페이지 바로가기🔗</div>
+        </Link>
+        <Link href={'/connect'}>
+          <div className="hover:text-gray-500">커플 연결 페이지 바로가기🔗</div>
+        </Link>
+        <Link href={'/calendar'}>
+          <div className="hover:text-gray-500">캘린더 페이지 바로가기🔗</div>
+        </Link>
+      </div>
+    </div>
+  )
 }

5422ad73f8fa9f9265940d099d715dcdeccd20b2
chipmunkcol <jack95711@gmail.com>
2025-02-14 23:17:09 +0900

    fix: Suspense 위치 변경

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import Oauth from '@/features/oauth'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 export default function Page() {
   // console.log('로그인 페이지 렌더링')
@@ -18,8 +19,15 @@ export default function Page() {
         </div>
         {/* 카카오 로그인 컴포넌트 */}
         {/* 카카오 로그인 컴포넌트 */}
-        <Oauth />
+        <Suspense>
+          <Oauth />
+        </Suspense>
       </div>
     </div>
   )
 }
+
+// Suspense 사용 이유
+// Suspense 경계 없이 검색 매개변수를 읽으면 useSearchParams()전체 페이지가 클라이언트 측 렌더링에 옵트인됩니다. 이로 인해 클라이언트 측 JavaScript가 로드될 때까지 페이지가 비어 있을 수 있습니다
+// git issue (https://github.com/vercel/next.js/discussions/61654)
+// 공식문서 내용 (https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout)
--- src/features/oauth/index.tsx
+++ src/features/oauth/index.tsx
@@ -1,8 +1,8 @@
-import { Suspense, useEffect } from 'react'
+import { useNavigater } from '@/shared/lib'
+import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 import UseLoginOauth from './lib/useLoginOauth'
 import LoginKakaoBtn from './ui/loginKakaoBtn'
-import { useSearchParams } from 'next/navigation'
-import { useNavigater } from '@/shared/lib'
 
 const Oauth = () => {
   const { loginController } = UseLoginOauth()
@@ -18,18 +18,13 @@ const Oauth = () => {
   }, [])
 
   return (
-    <Suspense>
+    <div className="text-center">
       <LoginKakaoBtn handler={loginController} />
       <div>
         <div className="underline text-sm text-gray-300">다른 이메일로 시작하기</div>
       </div>
-    </Suspense>
+    </div>
   )
 }
 
 export default Oauth
-
-// Suspense 사용 이유
-// Suspense 경계 없이 검색 매개변수를 읽으면 useSearchParams()전체 페이지가 클라이언트 측 렌더링에 옵트인됩니다. 이로 인해 클라이언트 측 JavaScript가 로드될 때까지 페이지가 비어 있을 수 있습니다
-// git issue (https://github.com/vercel/next.js/discussions/61654)
-// 공식문서 내용 (https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout)
